dce340ed7a1dfbcc62ca95e122bf57ad7d602a24
rashmiap <rashmiap.10@gmail.com>
2018-11-18 17:58:24 +0530

    added routers and paths

--- server.js
+++ server.js
@@ -3,26 +3,30 @@ var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+const db = require('./app/config/mongodb.env.js');
 
 var app = express();
 let uploadRouter = require('./app/routers/upload.router.js');
 let helloRouter = require('./app/routers/hello.router.js');
-
-app.use('/', uploadRouter);
-app.use('/', helloRouter);
+let caseStudiesRouter = require('./app/routers/CaseStudies.router.js');
 
 //connecting to mongodb
-mongoose.connect('mongodb://rashmiap:123456r@ds163103.mlab.com:63103/amygb-localdev-test', { useNewUrlParser: true });
+mongoose.connect(db.MONGO_DB_URI, { useNewUrlParser: true });
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(cors());
-
 app.use('/public', express.static(process.cwd() + '/public'));
+
+
+app.use('/api/fileanalyse', uploadRouter);
+app.use('/api/hello', helloRouter);
+app.use('/api/casestudies', caseStudiesRouter);
 
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
 });
-
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
 });
